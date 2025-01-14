52c3caf08452b52d33bb5959acdfa73d38d30128
nabilasusan <nabila_2226240101@mhs.mdp.ac.id>
2025-01-14 12:18:09 +0700

    update profile

--- hotel_palembang/lib/screens/profile_screens.dart
+++ hotel_palembang/lib/screens/profile_screens.dart
@@ -1,8 +1,52 @@
 import 'package:flutter/material.dart';
-import 'edit_profile_screen.dart'; // Import halaman Edit Profile yang akan dibuat
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  File? _profileImage;
+  String? _savedImagePath;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProfileImage();
+  }
+
+  Future<void> _loadProfileImage() async {
+    final prefs = await SharedPreferences.getInstance();
+    final imagePath = prefs.getString('profile_image_path');
+    if (imagePath != null) {
+      setState(() {
+        _savedImagePath = imagePath;
+        _profileImage = File(imagePath);
+      });
+    }
+  }
+
+  Future<void> _saveProfileImage(String path) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('profile_image_path', path);
+  }
+
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+      await _saveProfileImage(pickedFile.path);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,114 +56,141 @@ class ProfileScreen extends StatelessWidget {
         centerTitle: true,
         elevation: 0,
       ),
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Profile Picture
-                  CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('assets/images/profile.jpg'), // Gambar profil
-                  ),
-                  const SizedBox(height: 16),
-
-                  // Name
-                  const Text(
-                    'Eric Candra',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-
-                  // Email
-                  const Text(
-                    'ericcandra@gmail.com',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-
-                  // Phone Number
-                  const Text(
-                    '+62 812 1555 9999',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-
-                  // Additional Details
-                  const Divider(height: 1, thickness: 1, color: Colors.grey),
-                  const SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: const [
-                          Icon(Icons.cake, color: Colors.blue, size: 30),
-                          SizedBox(height: 8),
-                          Text(
-                            'Birthday',
-                            style: TextStyle(fontSize: 16, color: Colors.grey),
-                          ),
-                          Text(
-                            '29 Oktober 2001',
-                            style: TextStyle(fontSize: 16, color: Colors.black),
-                          ),
-                        ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade300, Colors.purple.shade300],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Profile Picture
+                    GestureDetector(
+                      onTap: _pickImage, // Mengganti foto profil saat gambar ditekan
+                      child: CircleAvatar(
+                        radius: 80,
+                        backgroundImage: _profileImage != null
+                            ? FileImage(_profileImage!)
+                            : const AssetImage('images/profile.jpg') as ImageProvider,
                       ),
-                      Column(
-                        children: const [
-                          Icon(Icons.location_city, color: Colors.blue, size: 30),
-                          SizedBox(height: 8),
-                          Text(
-                            'Kota',
-                            style: TextStyle(fontSize: 16, color: Colors.grey),
-                          ),
-                          Text(
-                            'Palembang',
-                            style: TextStyle(fontSize: 16, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24),
-
-                  // Edit Button
-                  ElevatedButton(
-                    onPressed: () {
-                      // Menavigasi ke halaman Edit Profile saat tombol ditekan
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => EditProfileScreen()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      backgroundColor: Colors.blue,
                     ),
-                    child: const Text('Edit Profile', style: TextStyle(color: Colors.white)),
-                  ),
-                ],
+                    const SizedBox(height: 16),
+
+                    // Name
+                    const Text(
+                      'Eric Candra',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+
+                    // Email
+                    const Text(
+                      'ericcandra@gmail.com',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white70,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+
+                    // Phone Number
+                    const Text(
+                      '+62 812 1555 9999',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white70,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+
+                    // Additional Details
+                    const Divider(height: 1, thickness: 1, color: Colors.white70),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: const [
+                            Icon(Icons.cake, color: Colors.white, size: 30),
+                            SizedBox(height: 8),
+                            Text(
+                              'Birthday',
+                              style: TextStyle(fontSize: 16, color: Colors.white70),
+                            ),
+                            Text(
+                              '29 Oktober 2001',
+                              style: TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Icon(Icons.location_city, color: Colors.white, size: 30),
+                            SizedBox(height: 8),
+                            Text(
+                              'Kota',
+                              style: TextStyle(fontSize: 16, color: Colors.white70),
+                            ),
+                            Text(
+                              'Palembang',
+                              style: TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+
+                    // Edit Button
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => EditProfileScreen()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Text('Edit Profile', style: TextStyle(color: Colors.blue)),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class EditProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+      ),
+      body: const Center(
+        child: Text('Edit Profile Screen Placeholder'),
       ),
     );
   }
